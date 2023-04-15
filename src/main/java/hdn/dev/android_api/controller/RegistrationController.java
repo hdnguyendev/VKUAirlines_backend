2a75b79cfe559fb7cc66778e6d8f11b0171d221f
hdn.2109 <nguyenhd.dev@gmail.com>
2023-04-15 12:25:29 +0700

    make it better

--- src/main/java/hdn/dev/android_api/controller/RegistrationController.java
+++ src/main/java/hdn/dev/android_api/controller/RegistrationController.java
@@ -8,8 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
@@ -19,16 +18,12 @@ import java.util.Optional;
 public class RegistrationController {
     @Autowired
     private UserRepository userRepository;
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+
     @PostMapping("/register")
     public ResponseEntity<ResponseObject> register(@RequestBody User user) throws JsonProcessingException {
-        if (userRepository.findByUsername(user.getUsername()) != null) {
+        if (userRepository.findByUsername(user.getUsername()).isPresent()) {
             return ResponseEntity.status(HttpStatus.OK).body(new ResponseObject("error", "Username is already taken!", ""));
         }
-
         User newUser = new User();
         newUser.setUsername(user.getUsername());
         newUser.setPassword(user.getPassword());
